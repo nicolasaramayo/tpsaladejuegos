7ff34e5b80d29c09b4595aa64ba471a351928cfe
nicolasaramayo <aramayo.fabian@gmail.com>
2022-09-05 01:16:40 -0300

    Creo los componentes basicos para la app

--- misaladejuegos/src/app/app.module.ts
+++ misaladejuegos/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SobreMiComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
