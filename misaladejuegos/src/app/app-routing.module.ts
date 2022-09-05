81e3c2485412c2f882c6eec2da3140851456607e
nicolasaramayo <aramayo.fabian@gmail.com>
2022-09-05 01:31:24 -0300

    Agrego el ruteo

--- misaladejuegos/src/app/app-routing.module.ts
+++ misaladejuegos/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// ruteo
+import { HomeComponent } from "./home/home.component";
+import { SobreMiComponent } from "./sobre-mi/sobre-mi.component";
+import { LoginComponent } from "./login/login.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: SobreMiComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
