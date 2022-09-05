41ba49b468f3b2b2c369ca74a41770ca7469707f
nicolasaramayo <aramayo.fabian@gmail.com>
2022-09-05 15:37:05 -0300

    Agrego el front del login y la animacion basica

--- misaladejuegos/src/app/login/login.component.ts
+++ misaladejuegos/src/app/login/login.component.ts
@@ -10,6 +10,20 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
 
+    if (signUpButton && container) {   
+      signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+      });
+    }
+
+    if (signInButton && container) {
+      signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+      });
+    }
+  }
 }
