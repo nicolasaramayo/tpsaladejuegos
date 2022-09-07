29a3bdf4687edae9e8920e0805d25c846847ad84
nicolasaramayo <aramayo.fabian@gmail.com>
2022-09-07 01:24:51 -0300

    Modifico el server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path'); 
-app.use(express.static(__dirname + '/angularpp'));
-
-app.listen(process.env.port || 8080);
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+app.use(express.static(__dirname + '/angularapp'));
+app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 console.log('Build successful!!');
