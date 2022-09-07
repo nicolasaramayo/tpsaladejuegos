7a3fb5f206f5e6072937736c0d53426d47f2a7af
nicolasaramayo <aramayo.fabian@gmail.com>
2022-09-07 02:06:34 -0300

    Replace titulo en todos los archivo de misaladejuegos a tpsaladejuegos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'misaladejuegos'`, () => {
+  it(`should have as title 'tpsaladejuegos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('misaladejuegos');
+    expect(app.title).toEqual('tpsaladejuegos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('misaladejuegos app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tpsaladejuegos app is running!');
   });
 });
