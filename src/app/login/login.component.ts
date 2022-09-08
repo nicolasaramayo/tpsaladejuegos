6ac425d25da28d983d90ad1bb2f2d02ac0ad57a4
nicolasaramayo <aramayo.fabian@gmail.com>
2022-09-08 19:05:10 -0300

    Cambio el front del login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
+//import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
+  //providers: [AuthService ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  //constructor(private authSvc: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
-
-    if (signUpButton && container) {   
-      signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-      });
-    }
-
-    if (signInButton && container) {
-      signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-      });
-    }
   }
+
+
+  async onLogin(){
+    const {email, password} = this.loginForm.value;
+    /*try {
+      const user = this.authSvc.login(email,password);
+      if (user) {
+        this.router.navigate(['/home']);
+      }
+    } catch (error) {
+      console.log(error);
+    }*/
+    
+  }
+
 }
